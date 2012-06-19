9d2272908e826a35c1440ddc9e6eedfe5464df1b
Aaron Goldman <goldman.git@gmail.com>
2012-06-18 20:36:32 -0400

    Refactored and Added Unit Test Project

--- SnoopBreadcrumbs/MainVM.cs
+++ SnoopBreadcrumbs/MainVM.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Xml;
 using System.IO;
-using System.Xml.Linq;
 using System.Windows;
 using System.Threading.Tasks;
 using System.Windows.Threading;
@@ -23,6 +21,7 @@ namespace SnoopBreadcrumbs
 
 
 
+        XmlHelper _xmlHelper = new XmlHelper();
         string _displayText;
 
         public string DisplayText
@@ -101,113 +100,6 @@ namespace SnoopBreadcrumbs
         }
 
 
-        const string LineNumberFormatTag = "{LineNo}";
-        HashSet<string> _requiresXPrefix = new HashSet<string>() { "xmlns", "Class" };
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="xmlSource"></param>
-        /// <param name="format">must include "{LineNo}" for Line Number spot</param>
-        /// <returns></returns>
-        private string TagXmlElements(string xmlSource, string format = LineNumberFormatTag)
-        {
-            format = format.Replace(LineNumberFormatTag, "{0}");
-            var frameworkElements = new AssemblyHelper()
-                .GetFrameworkElements().Select(fe => fe.Name);
-
-            StringBuilder output = new StringBuilder();
-            XmlWriterSettings ws = new XmlWriterSettings();
-            XmlWriter writer = null;
-
-            // Create an XmlReader
-            using (XmlTextReader reader =
-                new XmlTextReader(new StringReader(xmlSource)))
-            {
-                ws.ConformanceLevel = ConformanceLevel.Auto;
-
-                using (writer = XmlWriter.Create(output, ws))
-                {
-                    // Parse the file and display each of the nodes.
-                    while (reader.Read())
-                    {
-                        switch (reader.NodeType)
-                        {
-                            case XmlNodeType.Element:
-                                //writer.WriteAttributes(reader, false);
-                                var eName = reader.Name;
-                                var hasValue = !reader.IsEmptyElement;
-
-                                if (eName.Contains(":"))
-                                {
-                                    var split = eName.Split(':');
-                                    writer.WriteStartElement(split[0], split[1], null);
-                                }
-                                else
-                                {
-                                    writer.WriteStartElement(eName);
-                                }
-
-                                for (int i = 0; i < reader.AttributeCount; ++i)
-                                {
-                                    reader.MoveToNextAttribute();
-                                    var name = reader.Name;
-
-                                    if (this._requiresXPrefix.Contains(name))
-                                        name = "x:" + name;
-
-                                    if (name.Contains(":"))
-                                    {
-                                        var split = name.Split(':');
-                                        writer.WriteAttributeString(split[0], split[1], null, reader.Value);
-                                    }
-                                    else
-                                    {
-                                        writer.WriteAttributeString(name, reader.Value);
-                                    }
-
-                                }
-
-
-                                if (frameworkElements.Contains(eName))
-                                {
-                                    bool hasTag = reader.GetAttribute("Tag") != null;
-
-                                    if (!hasTag)
-                                        writer.WriteAttributeString("Tag",
-                                            string.Format(format, reader.LineNumber.ToString()));
-                                }
-
-                                if (!hasValue)
-                                {
-                                    writer.WriteEndElement();
-                                }
-                                break;
-                            case XmlNodeType.Text:
-                                writer.WriteString(reader.Value);
-                                break;
-                            case XmlNodeType.XmlDeclaration:
-                            case XmlNodeType.ProcessingInstruction:
-                                writer.WriteProcessingInstruction(reader.Name, reader.Value);
-                                break;
-                            case XmlNodeType.Comment:
-                                writer.WriteComment(reader.Value);
-                                break;
-                            case XmlNodeType.EndElement:
-                                    writer.WriteFullEndElement();
-                                break;
-                        }
-                    }
-
-                }
-            }
-
-
-            XDocument doc = XDocument.Parse(output.ToString());
-            return doc.ToString();
-        }
-
-
         public void ProcessXamls()
         {
 
@@ -278,8 +170,8 @@ namespace SnoopBreadcrumbs
 
                 var fileName = Path.GetFileName(file);
 
-                var newXaml = this.TagXmlElements(xaml,
-                    fileName + ": " + LineNumberFormatTag + " " + file);
+                var newXaml = this._xmlHelper.TagXmlElements(xaml,
+                    fileName + ": " + XmlHelper.LineNumberFormatTag + " " + file);
 
                 AddMessage("Writing: " + file);
 
---
+++ SnoopBreadcrumbs/XmlHelper.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace SnoopBreadcrumbs
+{
+    public class XmlHelper
+    {
+
+        public const string LineNumberFormatTag = "{LineNo}";
+        HashSet<string> _requiresXPrefix = new HashSet<string>() { "xmlns", "Class" };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xmlSource"></param>
+        /// <param name="format">must include "{LineNo}" for Line Number spot</param>
+        /// <returns></returns>
+        public string TagXmlElements(string xmlSource, string format = LineNumberFormatTag)
+        {
+            format = format.Replace(LineNumberFormatTag, "{0}");
+            var frameworkElements = new AssemblyHelper()
+                .GetFrameworkElements().Select(fe => fe.Name);
+
+            StringBuilder output = new StringBuilder();
+            XmlWriterSettings ws = new XmlWriterSettings();
+            XmlWriter writer = null;
+
+            // Create an XmlReader
+            using (XmlTextReader reader =
+                new XmlTextReader(new StringReader(xmlSource)))
+            {
+                ws.ConformanceLevel = ConformanceLevel.Auto;
+
+                using (writer = XmlWriter.Create(output, ws))
+                {
+                    // Parse the file and display each of the nodes.
+                    while (reader.Read())
+                    {
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                //writer.WriteAttributes(reader, false);
+                                var eName = reader.Name;
+                                var hasValue = !reader.IsEmptyElement;
+
+                                if (eName.Contains(":"))
+                                {
+                                    var split = eName.Split(':');
+                                    writer.WriteStartElement(split[0], split[1], null);
+                                }
+                                else
+                                {
+                                    writer.WriteStartElement(eName);
+                                }
+
+                                for (int i = 0; i < reader.AttributeCount; ++i)
+                                {
+                                    reader.MoveToNextAttribute();
+                                    var name = reader.Name;
+
+                                    if (this._requiresXPrefix.Contains(name))
+                                        name = "x:" + name;
+
+                                    if (name.Contains(":"))
+                                    {
+                                        var split = name.Split(':');
+                                        writer.WriteAttributeString(split[0], split[1], null, reader.Value);
+                                    }
+                                    else
+                                    {
+                                        writer.WriteAttributeString(name, reader.Value);
+                                    }
+
+                                }
+
+
+                                if (frameworkElements.Contains(eName))
+                                {
+                                    bool hasTag = reader.GetAttribute("Tag") != null;
+
+                                    if (!hasTag)
+                                        writer.WriteAttributeString("Tag",
+                                            string.Format(format, reader.LineNumber.ToString()));
+                                }
+
+                                if (!hasValue)
+                                {
+                                    writer.WriteEndElement();
+                                }
+                                break;
+                            case XmlNodeType.Text:
+                                writer.WriteString(reader.Value);
+                                break;
+                            case XmlNodeType.XmlDeclaration:
+                            case XmlNodeType.ProcessingInstruction:
+                                writer.WriteProcessingInstruction(reader.Name, reader.Value);
+                                break;
+                            case XmlNodeType.Comment:
+                                writer.WriteComment(reader.Value);
+                                break;
+                            case XmlNodeType.EndElement:
+                                writer.WriteFullEndElement();
+                                break;
+                        }
+                    }
+
+                }
+            }
+
+
+            XDocument doc = XDocument.Parse(output.ToString());
+            return doc.ToString();
+        }
+
+    }
+}
