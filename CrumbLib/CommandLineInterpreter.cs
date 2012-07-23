9ad60da4331e2c1186693f3eccf1e79b9615c420
Aaron Goldman <goldman.git@gmail.com>
2012-07-22 21:46:12 -0400

    Command Line Interpreter

---
+++ CrumbLib/CommandLineInterpreter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrumbLib
+{
+    public class CommandLineInterpreter
+    {
+
+        public CommandLineInterpreter()
+        {
+            Ignores = new List<string>();
+
+        }
+
+        public bool Parse(string[] args)
+        {
+            if (args == null || args.Length == 0)
+                return false;
+
+            Path = args[0];
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                Ignores.Add(args[i]);                
+            }
+
+            return true;
+        }
+
+
+        public List<string> Ignores { get; private set; }
+        public string Path { get; private set; }
+    }
+}
---
+++ Tester/CommandLineInterpreterTester.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrumbLib;
+using NUnit.Framework;
+
+namespace Tester
+{
+    [TestFixture]
+    public class CommandLineInterpreterTester
+    {
+
+        CommandLineInterpreter target;
+        
+        [SetUp]
+        public void Init()
+        {
+            target = new CommandLineInterpreter();
+        }
+
+
+        [Test]
+        public void ParseCommandLine()
+        {
+            var path = @"C:\data\prog\target" ;
+
+            var result = target.Parse(new[] { path });
+
+            Assert.AreEqual(path, target.Path);
+            Assert.AreEqual(0, target.Ignores.Count);
+
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        public void ParseIgnores()
+        {
+            var path = @"C:\data\prog\target";
+
+            var result = target.Parse(new[] { path, "A", "B", "X" });
+
+            Assert.AreEqual(path, target.Path);
+            Assert.AreEqual(3, target.Ignores.Count);
+
+            Assert.AreEqual("A", target.Ignores[0]);
+            Assert.AreEqual("B", target.Ignores[1]);
+            Assert.AreEqual("X", target.Ignores[2]);
+
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        public void ParseFailNull()
+        {
+            var result = target.Parse(null);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ParseFailEmpty()
+        {
+            var result = target.Parse(new string[]{});
+            Assert.IsFalse(result);        
+        }
+    }
+}
