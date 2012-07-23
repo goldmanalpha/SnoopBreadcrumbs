702e661edc5007d36b3b6bbeb1fcf6561c470e34
Aaron Goldman <goldman.git@gmail.com>
2012-07-22 22:57:48 -0400

    Console Complete - Untested

---
+++ SnoopBCConsole/ConsoleMessageHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrumbLib;
+
+namespace SnoopBCConsole
+{
+    class ConsoleMessageHelper : IMessageHandler
+    {
+        public void AddMessage(string message, bool isStatus = true, bool isDetail = true)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- SnoopBCConsole/Program.cs
+++ SnoopBCConsole/Program.cs
@@ -11,22 +11,46 @@ namespace SnoopBCConsole
         static int Main(string[] args)
         {
             var interpreter = new CommandLineInterpreter();
+            var msgHelper = new ConsoleMessageHelper();
+                
 
             if (!interpreter.Parse(args))
             {
-                Console.WriteLine("Usage: SnoopBCConsole RootPath [ignoreControl1] [ignoreControl2] [ignoreControl3] [...]");
+                msgHelper.AddMessage("Usage: SnoopBCConsole RootPath [ignoreControl1] [ignoreControl2] [ignoreControl3] [...]");
                 return 1;
             }
             else
             {
 
-                var helper = new XmlHelper();
+                var processor = new XamlFilesProcessor();
 
-                //helper.TagXmlElements(interpreter.
-            
+                var xamls = processor.FindFiles(interpreter.Path, 
+                    (s, s1) => msgHelper.AddMessage(s + ": " + s1), msgHelper);
+
+                if (xamls.Count == 0)
+                {
+                    msgHelper.AddMessage("No Xaml files found");
+                    return 2;
+                }
+                else
+                {
+                    var exceptions = processor.ProcessXamls(xamls, 
+                        (s, s1) => msgHelper.AddMessage(s + ": " + s1), i => {}, 
+                        msgHelper);
+
+                    if (exceptions > 0)
+                    {
+                        msgHelper.AddMessage("Exception count: " + exceptions);
+                        return 3;
+                    }
+                }
             }
 
             return 0;
         }
+
+        
     }
+
+
 }
