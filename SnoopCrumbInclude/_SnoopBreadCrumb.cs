e8681118d789be3a73deeddea201418d18fe2e6f
Aaron Goldman <goldman.git@gmail.com>
2012-11-22 18:46:28 -0500

    Added Attachable Property

---
+++ SnoopCrumbInclude/_SnoopBreadCrumb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+
+namespace SnoopCrumbInclude
+{
+    public class _SnoopBreadCrumb
+    {
+        public static readonly DependencyProperty _BreadCrumbProperty = DependencyProperty.RegisterAttached(
+              "_BreadCrumb",
+              typeof(string),
+              typeof(_SnoopBreadCrumb),
+              new FrameworkPropertyMetadata(null)
+            );
+
+        public static void Set_BreadCrumb(UIElement element, string value)
+        {
+            element.SetValue(_BreadCrumbProperty, value);
+        }
+        public static string Get_BreadCrumb(UIElement element)
+        {
+            return (string)element.GetValue(_BreadCrumbProperty);
+        }
+    }
+}
