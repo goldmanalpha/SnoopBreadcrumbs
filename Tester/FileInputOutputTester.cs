14c7454d352ea8c9ad1e24747a3d79e7c52dd572
Aaron Goldman <goldman.git@gmail.com>
2012-11-22 19:36:27 -0500

    Refactored To Support Multiple Directories/Test types

---
+++ Tester/FileInputOutputTester.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tester
+{
+    public abstract class FileInputOutputTester
+    {
+
+
+        List<string> _files;
+        protected List<string> files { 
+            get {
+
+                if (_files == null)
+                    _files =  System.IO.Directory
+                        .GetFiles(testDocFolder())
+                        .ToList();
+
+                return _files.ToList();
+        } }
+
+        protected abstract string testDocFolder();
+        protected abstract string FileProcessingFunction(string contents);
+
+        protected void TestSingle(string testName)
+        {
+            var filesIn = files.Where(f => f == testDocFolder() + "\\" + testName + ".Input.xml");
+
+            if (!filesIn.Any())
+            {
+                Assert.Fail("No files for test: " + testName);
+            }
+
+            foreach (var inFile in filesIn)
+            {
+                var xmlIn = LoadFile(inFile);
+
+                var outFile = inFile.Replace(".Input.xml", ".Output.xml");
+                var xmlOut = System.IO.File.Exists(outFile) ? LoadFile(outFile) : null;
+
+                var processed = this.FileProcessingFunction(xmlIn);
+
+                //for outputting expected values:
+                //System.IO.File.WriteAllText(outFile, tagged);
+
+                Assert.AreEqual(xmlOut, processed, "Tagged xml doesn't match: " + outFile);
+
+                //in case the source needs a modification                
+                //System.IO.File.WriteAllText(inFile, xmlIn);
+
+            }
+        }
+
+        /// <summary>
+        /// checks all the files to make sure nothing was forgotten
+        /// </summary>
+        public  virtual void TestAllInputOutput()
+        {
+            var TestIn = files.Where(f => f.EndsWith(".Input.xml"))
+                .Select(f => f.Replace(".Input.xml", ""))
+                .Select(f => f.Split(new[] { "\\" }, StringSplitOptions.None).Last());
+
+            foreach (var test in TestIn)
+            {
+                this.TestSingle(test);
+            }
+        }
+
+
+        protected string LoadFile(string fileName)
+        {
+            return System.IO.File.ReadAllText(fileName);
+        }
+
+    }
+}
---
+++ Tester/XmlHelperXamlTester.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CrumbLib;
+
+
+namespace Tester
+{
+    [TestFixture]
+    public class XmlHelperXamlTester : FileInputOutputTester
+    {
+        XmlHelper helper = new XmlHelper();
+
+
+        protected override string testDocFolder()
+        {
+            return "InputOutput";
+        }
+
+        protected override string FileProcessingFunction(string contents)
+        {
+            return helper.TagXmlElements(
+                contents, 
+                s => { }, "**value**" + XmlHelper.LineNumberFormatTag + "**end**", 
+                new List<string>());
+        }
+
+        [Test]
+        public void CanConstruct()
+        {
+            var x = new XmlHelper();
+        }
+        
+        [Test]
+        public void ExpandedTag()
+        {
+            this.TestSingle("ExpandedTag");
+        }
+
+        [Test]
+        public void SimpleSetTag()
+        {
+            this.TestSingle("SimpleSet");
+        }
+
+        [Test]
+        public void Xml1()
+        {
+            this.TestSingle("Xml1");
+        }
+
+        [Test]
+        public void Xml1UnexpectedOrder()
+        {
+            this.TestSingle("Xml1.UnexpectedOrder");
+        }
+
+        [Test]
+        public void MCIgnorable()
+        {
+            this.TestSingle("MC.Ignorable");
+        }
+
+        [Test]
+        [Ignore]
+        public void XClass()
+        { 
+            this.TestSingle("XClass");
+        }
+
+
+        [Test]
+        [Ignore]
+        public void XmlNS()
+        {
+            this.TestSingle("XmlNS");
+        }
+
+        [Test]
+        public void UnexpectedRootTagReturnsUnchanged()
+        {
+            this.TestSingle("Xml1.UnexpectedRootTag");
+        }
+
+        [Test]
+        public void MultiNamespaces()
+        {
+            this.TestSingle("MultiNamespaces");
+        }
+
+                /// <summary>
+        /// checks all the files to make sure nothing was forgotten
+        /// </summary>
+        [Test]
+        public override void TestAllInputOutput()
+        {
+            base.TestAllInputOutput();
+        }
+
+
+
+    }
+}
